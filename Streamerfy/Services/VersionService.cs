2e39b73a605e227aeea0eb79d8e1f75e0a04332b
Blaze <35648047+WTFBlaze@users.noreply.github.com>
2025-04-11 03:02:26 -0400

    Bumped Version to 1.0.3

--- Streamerfy/Services/VersionService.cs
+++ Streamerfy/Services/VersionService.cs
@@ -6,7 +6,7 @@ namespace Streamerfy.Services
     public static class VersionService
     {
         private const string LatestReleaseApi = "https://api.github.com/repos/WTFBlaze/Streamerfy/releases/latest";
-        public const string CurrentVersion = "1.0.2";
+        public const string CurrentVersion = "1.0.3";
 
         public static async Task<(bool isUpdateAvailable, string latestVersion)> CheckForUpdatesAsync()
         {
