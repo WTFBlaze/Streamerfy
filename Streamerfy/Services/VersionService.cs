d92e846f0d6f6eb77ab4469d13dab9b2b27afba9
Blaze <35648047+WTFBlaze@users.noreply.github.com>
2025-04-12 18:13:33 -0400

    Temporary Test Version Bump (Will remove after test completed)

--- Streamerfy/Services/VersionService.cs
+++ Streamerfy/Services/VersionService.cs
@@ -6,7 +6,7 @@ namespace Streamerfy.Services
     public static class VersionService
     {
         private const string LatestReleaseApi = "https://api.github.com/repos/WTFBlaze/Streamerfy/releases/latest";
-        public const string CurrentVersion = "1.0.6";
+        public const string CurrentVersion = "1.0.6-Test";
 
         public static async Task<(bool isUpdateAvailable, string latestVersion)> CheckForUpdatesAsync()
         {
